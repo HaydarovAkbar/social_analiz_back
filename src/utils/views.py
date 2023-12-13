f95d05faf864990d6e55349e508c44db57912662
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2023-12-13 10:08:21 +0500

    change utils/views.py views order by id

--- src/utils/views.py
+++ src/utils/views.py
@@ -11,39 +11,39 @@ from . import serializers
 
 
 class StateView(viewsets.ModelViewSet):
-    queryset = models.State.objects.all()
+    queryset = models.State.objects.all().order_by('id')
     serializer_class = serializers.StateSerializers
     pagination_class = TenPagination
     http_method_names = ['get', ]
 
 
 class LanguageView(viewsets.ModelViewSet):
-    queryset = models.Language.objects.all()
+    queryset = models.Language.objects.all().order_by('id')
     serializer_class = serializers.LanguageSerializers
     pagination_class = TenPagination
     http_method_names = ['get', ]
 
 
 class CategoryView(viewsets.ModelViewSet):
-    queryset = models.Category.objects.all()
+    queryset = models.Category.objects.all().order_by('id')
     serializer_class = serializers.CategorySerializers
     pagination_class = TenPagination
 
 
 class SpecializationView(viewsets.ModelViewSet):
-    queryset = models.Specialization.objects.all()
+    queryset = models.Specialization.objects.all().order_by('id')
     serializer_class = serializers.SpecializationSerializers
     pagination_class = TenPagination
 
 
 class RegionView(viewsets.ModelViewSet):
-    queryset = models.Region.objects.all()
+    queryset = models.Region.objects.all().order_by('id')
     serializer_class = serializers.RegionSerializers
     pagination_class = TenPagination
 
 
 class DistrictView(viewsets.ModelViewSet):
-    queryset = models.District.objects.all()
+    queryset = models.District.objects.all().order_by('id')
     serializer_class = serializers.DistrictSerializers
     pagination_class = TenPagination
     filter_backends = [DjangoFilterBackend, ]
