53d229921449dfc128a37b6ebc4d1a0191fd7bc9
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2023-12-19 20:44:02 +0500

    change requirements.txt

---
+++ src/television/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TelevisionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'television'
---
+++ src/utils/minio/instruction.py
@@ -0,0 +1,3 @@
+from minio import Minio
+
+
