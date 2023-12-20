01cc5e125c83a5202cd1c5eac37225ec551a8aa9
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2023-12-20 15:36:30 +0500

    television models

--- src/television/models.py
+++ src/television/models.py
@@ -1,3 +1,76 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
+
+class FileStatus(models.Model):
+    name = models.CharField(max_length=255, verbose_name=_('Name'))
+    state = models.ForeignKey('utils.State', on_delete=models.SET_NULL, null=True, blank=True,
+                              related_name='file_statuses',
+                              verbose_name=_('State'))
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('File status')
+        verbose_name_plural = _('File statuses')
+        db_table = 'file_status'
+
+
+class TelevisionType(models.Model):
+    name = models.CharField(max_length=255, verbose_name=_('Name'))
+    state = models.ForeignKey('utils.State', on_delete=models.SET_NULL, null=True, blank=True,
+                              related_name='television_types',
+                              verbose_name=_('State'))
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Television type')
+        verbose_name_plural = _('Television types')
+        db_table = 'television_type'
+
+
+class Files(models.Model):
+    file_name = models.CharField(max_length=250)
+    file_id = models.CharField(max_length=250, null=True, blank=True)
+    file_extension = models.CharField(max_length=250, null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+    state = models.ForeignKey('utils.State', on_delete=models.SET_NULL, null=True, blank=True,
+                              related_name='instruction', )
+
+    organization = models.ForeignKey('organization.Organization', on_delete=models.SET_NULL, null=True, blank=True,
+                                     related_name='files', verbose_name=_('Organization'))
+    post_date = models.DateField(null=True, blank=True, verbose_name=_('Post date'))
+    television_type = models.ForeignKey(TelevisionType, on_delete=models.SET_NULL, null=True, blank=True,
+                                        related_name='files', verbose_name=_('Television type'))
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.file_name
+
+    class Meta:
+        verbose_name = _('File')
+        verbose_name_plural = _('Files')
+        db_table = 'files'
+        indexes = [
+            models.Index(fields=['file_name', ]),
+            models.Index(fields=['post_date', ]),
+        ]
+
+    def save(self, *args, **kwargs):
+        self.updated_at = timezone.now()
+        super(Files, self).save(*args, **kwargs)
+        return self
