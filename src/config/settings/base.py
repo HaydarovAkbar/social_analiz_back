a70cbf2802b18184baa499dff443904fef1cdca6
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2023-12-21 12:57:29 +0500

    change coed

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -227,7 +227,7 @@ CORS_ALLOW_HEADERS = [
     'Access-Control-Allow-Headers',
     'Access-Control-Allow-Credentials',
 ]
-HOST = 'https://c969-194-93-24-3.ngrok-free.app'
+HOST = 'https://167d-194-93-24-3.ngrok-free.app'
 
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
--- src/utils/views.py
+++ src/utils/views.py
@@ -1,29 +1,27 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework import status
 from django_filters.rest_framework import DjangoFilterBackend
 
 from . import models
-from .pagination import TenPagination, TwentyPagination
+from .pagination import TenPagination
 from . import serializers
 
 
 class StateView(viewsets.ModelViewSet):
-    queryset = models.State.objects.all().order_by('id')
+    queryset = models.State.objects.all()
     serializer_class = serializers.StateSerializers
     pagination_class = TenPagination
     http_method_names = ['get', ]
 
 
 class LanguageView(viewsets.ModelViewSet):
-    queryset = models.Language.objects.all().order_by('id')
+    queryset = models.Language.objects.all()
     serializer_class = serializers.LanguageSerializers
     pagination_class = TenPagination
     http_method_names = ['get', ]
 
 
 class CategoryView(viewsets.ModelViewSet):
-    queryset = models.Category.objects.all().order_by('id')
+    queryset = models.Category.objects.all()
     serializer_class = serializers.CategorySerializers
     pagination_class = TenPagination
 
@@ -35,13 +33,13 @@ class SpecializationView(viewsets.ModelViewSet):
 
 
 class RegionView(viewsets.ModelViewSet):
-    queryset = models.Region.objects.all().order_by('id')
+    queryset = models.Region.objects.all()
     serializer_class = serializers.RegionSerializers
     pagination_class = TenPagination
 
 
 class DistrictView(viewsets.ModelViewSet):
-    queryset = models.District.objects.all().order_by('id')
+    queryset = models.District.objects.all()
     serializer_class = serializers.DistrictSerializers
     pagination_class = TenPagination
     filter_backends = [DjangoFilterBackend, ]
