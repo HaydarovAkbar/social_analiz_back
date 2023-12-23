c17d55c63b526a17555fc6fdaced35814634880f
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2023-12-23 14:10:56 +0500

    change serializers.py

--- src/rate/serializers.py
+++ src/rate/serializers.py
@@ -1,3 +1,9 @@
 from rest_framework import serializers
 
 from . import models
+
+
+class RatingCriteriaSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = models.RatingCriteria
+        fields = '__all__'
